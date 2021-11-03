2bd345b85ef82e557beac3fdb2215f5cb3ff6580
Ubuntu <ubuntu@ip-10-0-4-200.ec2.internal>
2021-11-03 15:13:57 +0000

    updated search node for v1/v2 of template 1

--- packages/nodes-base/nodes/Vidrovr/Search/VidrovrSearch.node.ts
+++ packages/nodes-base/nodes/Vidrovr/Search/VidrovrSearch.node.ts
@@ -6,12 +6,16 @@ import {
 
 import {
 	IDataObject,
+	ILoadOptionsFunctions,
 	INodeExecutionData,
+	INodePropertyOptions,
 	INodeType,
 	INodeTypeDescription,
+	NodeApiError,
 } from 'n8n-workflow';
 
 import { OptionsWithUri } from 'request';
+import { getEntityFields } from '../../Microsoft/Dynamics/GenericFunctions';
 
 export class VidrovrSearch implements INodeType {
 	description: INodeTypeDescription = {
@@ -40,9 +44,67 @@ export class VidrovrSearch implements INodeType {
 					displayName: 'Query',
 					name: 'query',
 					type: 'string',
-					default: 'Xi, CCP, China',
+					default: '',
 					required: true,
 					description: 'Keywords to search on'
+				},
+				// Part of the advanced Filter field
+				{
+					displayName: 'Recognized People',
+					name: 'recognized_people',
+					type: 'string',
+					default: '',
+					placeholder: 'Xi Jinping Foreman, Emma Stone',
+					description: 'Comma-separated list of people to search for'
+				},
+				{
+					displayName: 'Video Source',
+					name: 'video_source',
+					type: 'options',
+					default: 'direct_upload',
+					description: 'Type of video uploaded',
+					options: [
+						{
+							name: 'Feed',
+							value: 'feed',
+						},
+						{
+							name: 'Direct Upload',
+							value: 'direct_upload',
+						}
+					]
+				},
+				{
+					displayName: 'Feed Name',
+					name: 'feed_uid',
+					type: 'options',
+					default: '',
+					required: true,
+					displayOptions: {
+						show: {
+							video_source: [
+								'feed',
+							],
+						},
+					},
+					typeOptions: {
+						loadOptionsMethod: 'getFeeds',
+					},
+					description: 'Query will include the name of the feed set here'
+				},
+				{
+					displayName: 'Start Date',
+					name: 'start_date',
+					type: 'dateTime',
+					default: '',
+					description: 'Only query videos/feeds from after this date (inclusive)'
+				},
+				{
+					displayName: 'End Date',
+					name: 'end_date',
+					type: 'dateTime',
+					default: '',
+					description: 'Only query videos/feeds from before this date (inclusive)'
 				},
 				{
 					displayName: 'Additional Fields',
@@ -51,22 +113,6 @@ export class VidrovrSearch implements INodeType {
 					placeholder: 'Add Field',
 					default: {},
 					options: [
-						{
-							displayName: 'Filter',
-							name: 'filter',
-							type: 'json',
-							default: '',
-							required: false,
-							description: 'JSON-stringified filter parameters for the advanced search.'
-						},
-						{
-							displayName: 'Range',
-							name: 'range_query',
-							type: 'json',
-							default: '',
-							required: false,
-							description: 'JSON-formatted time-range query'
-						},
 						{
 							displayName: 'Size',
 							name: 'size',
@@ -98,16 +144,102 @@ export class VidrovrSearch implements INodeType {
 		]
 	};
 
+	methods = {
+		loadOptions: {
+			async getFeeds(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]>{
+				let returnData: INodePropertyOptions[] = [];
+				const credentials = await this.getCredentials('vidrovrApi') as IDataObject;
+				const api_uri = `https://api.vidrovr.com/feeds/feeds/get_feeds/?api_key=${credentials.apiKey}`;
+
+				const options: OptionsWithUri = {
+					method: 'GET',
+					uri: api_uri,
+					json: true,
+				};
+				if(this.helpers.request){
+					const { data } = await this.helpers.request(options);
+					for(const feed of data){
+						const feed_name = feed.name;
+						const feed_id = feed.uid;
+						returnData.push({
+							name: feed_name,
+							value: feed_id
+						});
+					}
+				}
+				return returnData;
+				
+			}
+		}
+	}
+
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		let responseData;
 		//Get credentials the user provided for this node
 		const credentials = await this.getCredentials('vidrovrApi') as IDataObject;
 
-		// Fields with defaults
+		// Request body. Includes:
+			// range: object for filtering creation_date
+			// exact: object for filtering by feed id
+		const body : {[k: string]: object} = {};
+
+		// Query string filters. Includes: 
+			// recognized_people: string
+		const filters: {[k: string]: any} = {};
+		
+		// Date range 
+		const date_range: {[k: string]: object} = {};
+
+		// Basic query
 		const query = this.getNodeParameter('query', 0) as string;
 
 		let api_uri = `https://api.vidrovr.com/search/search?api_key=${credentials.apiKey}&query=${query}`
+
+		// Adding fields to filters query string param
+		
+		// Recognized People
+		const recognized_people = this.getNodeParameter('recognized_people', 0) as string;
+		if(recognized_people.length > 0){
+			filters["recognized_people"]  = recognized_people.split(",");
+		}
+		if(Object.keys(filters).length > 0){
+			api_uri += `&filters=${JSON.stringify(filters)}`;
+		}
+
+		// Adding fields to request body
+		
+		// Video Source
+		const video_source = this.getNodeParameter('video_source', 0) as string;
+		if(video_source == "feed"){
+			const feed_uid = this.getNodeParameter('feed_uid', 0) as string;
+			body['exact'] = {
+				feed_uid: feed_uid
+			};
+		}
+
+		// Ramge for creation_date
+		let start_date = this.getNodeParameter('start_date', 0) as string;
+		let end_date = this.getNodeParameter('end_date', 0) as string;
+		const creation_date: {[k: string]: string} = {};
+
+		// 2021-11-02T04:00:00.000Z
+		if(start_date.length > 0){
+			start_date = start_date.split("T")[0] + "T00:00:00";
+			creation_date["gte"] = start_date;
+		}
+		if(end_date.length > 0){
+			end_date = end_date.split("T")[0] + "T00:00:00";
+			creation_date["lte"] = end_date;
+		}
+		if(Object.keys(creation_date).length > 0){
+			date_range['creation_date'] = creation_date
+		}
+
+		// Add creation date object to body
+		if(Object.keys(date_range).length > 0){
+			body['range'] = date_range
+		}
 
 		// Additional fields (as set in description field above) that are actually filled get grouped together to easily fetch optional params
 		const additionalFields = this.getNodeParameter('additionalFields', 0) as IDataObject;
@@ -116,14 +248,15 @@ export class VidrovrSearch implements INodeType {
 		for (const [key, value] of Object.entries(additionalFields)) {
 			api_uri += `&${key}=${value}`;
 		}
-
-		//Make http request 
 		const options: OptionsWithUri = {
 			method: 'GET',
 			uri: api_uri,
 			json: true,
+			body: body
 		};
-
+		console.log("API uri:", api_uri);
+		console.log("Body:", body);
+		
 		responseData = await this.helpers.request(options);
 
 		// Map data to n8n data
